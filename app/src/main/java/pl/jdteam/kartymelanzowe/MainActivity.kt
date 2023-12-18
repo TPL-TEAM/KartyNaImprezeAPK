db7fd72cca6d55c832ff043181001ad1c3d3007e
Aro000o <arekcichy2004@wp.pl>
2023-12-18 22:35:27 +0100

    Dodanie Drugiej Aktywności

--- app/src/main/java/pl/jdteam/kartymelanzowe/MainActivity.kt
+++ app/src/main/java/pl/jdteam/kartymelanzowe/MainActivity.kt
@@ -2,9 +2,13 @@ package pl.jdteam.kartymelanzowe
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import pl.jdteam.kartymelanzowe.databinding.ActivityMainBinding
+
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
