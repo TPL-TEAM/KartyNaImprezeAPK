d1cb8bcccccc9ec163b4a21874f392f0ba44275e
Awelol <konrad069@o2.pl>
2023-12-29 01:19:09 +0100

    29.12.2023 - brak blokady anty podwojne wyzwanie

--- app/src/main/java/pl/jdteam/kartymelanzowe/GameplayAc.kt
+++ app/src/main/java/pl/jdteam/kartymelanzowe/GameplayAc.kt
@@ -13,9 +13,38 @@ class GameplayAc : ComponentActivity() {
         binding = ActivityGameplayBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val array: Array<String> = resources.getStringArray(R.array.pytania)
-        var lista = array.toCollection(ArrayList())
+        val lista = array.toCollection(ArrayList())
         var losowe = Random.nextInt(0, lista.size-1)
+
+        val array1: Array<String> = resources.getStringArray(R.array.wyzwania)
+        val lista1 = array1.toCollection(ArrayList())
+        var losowe1 = Random.nextInt(0, lista1.size-1)
+
         binding.karta.text = lista[losowe]
+
+        binding.karta.setOnClickListener(){
+            binding.karta.text = lista1[losowe1]
+            if(lista1.isNotEmpty()){
+                lista1.removeAt(losowe1)
+                if(binding.karta.text != lista1[losowe1]) {
+                    if (lista1.size == 1) {
+
+                        binding.karta.text = lista1[0]
+                        lista1.clear()
+
+                    } else {
+                        losowe1 = Random.nextInt(0, lista1.size - 1)
+                        binding.karta.text = lista1[losowe1]
+
+                    }
+                }
+            }else{binding.karta.text = "Brak wyzwań!"}
+
+        }
+
+
+
+
 
         binding.nieWykonane.setOnClickListener(){
             if(lista.isNotEmpty()){
