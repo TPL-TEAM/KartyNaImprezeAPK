e8cb7ab019ff2715cb9b73d8317ea77712e8787d
Awelol <konrad069@o2.pl>
2024-01-22 22:07:40 +0100

    22.01.2024 - aktywnosc graczy

---
+++ app/src/main/java/pl/jdteam/kartymelanzowe/StartAc.kt
@@ -0,0 +1,12 @@
+package pl.jdteam.kartymelanzowe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class StartAc : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uzytkownicy)
+    }
+}
