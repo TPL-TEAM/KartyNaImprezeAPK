3080d7eecb7e53b9015ebc4553a7bc405056940a
Awelol <konrad069@o2.pl>
2024-01-30 18:49:51 +0100

    30.01.2024 - aktywnosc graczy 2

--- app/src/main/java/pl/jdteam/kartymelanzowe/StartAc.kt
+++ app/src/main/java/pl/jdteam/kartymelanzowe/StartAc.kt
@@ -3,13 +3,13 @@ package pl.jdteam.kartymelanzowe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import pl.jdteam.kartymelanzowe.databinding.ActivityMainBinding
+import pl.jdteam.kartymelanzowe.databinding.ActivityUzytkownicyBinding
 
 class StartAc : ComponentActivity() {
-    private lateinit var binding: ActivityStartBinding
+    private lateinit var binding: ActivityUzytkownicyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityStartBinding.inflate(layoutInflater)
+        binding = ActivityUzytkownicyBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
