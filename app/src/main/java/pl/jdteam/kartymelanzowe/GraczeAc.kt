1fd2f2e1db08238e412cd9632032cecb8ceae673
Aro00o <arekcichy2004@wp.pl>
2024-09-12 20:41:29 +0200

    Jakies poprawki moze działajacy system graczy(narazie tylko 2)

--- app/src/main/java/pl/jdteam/kartymelanzowe/GraczeAc.kt
+++ app/src/main/java/pl/jdteam/kartymelanzowe/GraczeAc.kt
@@ -2,6 +2,9 @@ package pl.jdteam.kartymelanzowe
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.activity.ComponentActivity
 import pl.jdteam.kartymelanzowe.databinding.ActivityGraczeBinding
 
@@ -17,15 +20,26 @@ class GraczeAc : ComponentActivity() {
             var dzikie_karty_switch = binding.switch1.isChecked
             var reverse_switch = binding.switch2.isChecked
             val startgra = Intent(applicationContext, GameplayAc::class.java)
-
             startgra.putExtra("DZIKIE_KARTY", dzikie_karty_switch)
             startgra.putExtra("REVERSE", reverse_switch)
             startActivity(startgra)
-
-
-
-
-
         }
+        binding.gracz1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrBlank()) {
+                    binding.gracz2.visibility = View.VISIBLE
+                } else {
+                    binding.gracz2.visibility = View.GONE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Empty implementation
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed for this case
+            }
+        })
     }
 }
