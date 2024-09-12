029005e72399f0502139ac878f836ad11e0e55bc
Aro00o <arekcichy2004@wp.pl>
2024-09-12 17:33:22 +0200

    Przerobiony system ładowania pytań Podpiecie wyboru kategorii

--- app/src/main/java/pl/jdteam/kartymelanzowe/GameplayAc.kt
+++ app/src/main/java/pl/jdteam/kartymelanzowe/GameplayAc.kt
@@ -2,8 +2,6 @@ package pl.jdteam.kartymelanzowe
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.activity.ComponentActivity
@@ -22,64 +20,101 @@ class GameplayAc : ComponentActivity() {
 
 
 
-        val ogolne: Array<String> = resources.getStringArray(R.array.ogolne)
-        val pytania: Array<String> = resources.getStringArray(R.array.pytaniaa)
-        var pytania_lista = pytania.toCollection(ArrayList())
+        val pytania = ArrayList<String>()
+        val wyzwania = ArrayList<String>()
+
 
         if(intent.getBooleanExtra("DZIKIE_KARTY", false) == true){
             val dzikie_karty: Array<String> = resources.getStringArray(R.array.dzikie_karty)
-            var dzikie_karty_lista = dzikie_karty.toCollection(ArrayList())
-            pytania_lista.addAll(dzikie_karty_lista)
+            val pytania_lista = dzikie_karty.toCollection(ArrayList())
+            pytania.addAll(pytania_lista)
         }
 
-        var losowe = Random.nextInt(0, pytania_lista.size-1)
+        if(intent.getBooleanExtra("KATEGORIA1CHECK", false) == true){
+            val pytania_ogolne: Array<String> = resources.getStringArray(R.array.pytania_ogolne)
+            val pytania_lista = pytania_ogolne.toCollection(ArrayList())
+            pytania.addAll(pytania_lista)
 
-        val wyzwania: Array<String> = resources.getStringArray(R.array.wyzwania)
-        val wyzwania_lista = wyzwania.toCollection(ArrayList())
+            val wyzwania_ogolne: Array<String> = resources.getStringArray(R.array.wyzwania_ogolne)
+            val wyzwania_lista = wyzwania_ogolne.toCollection(ArrayList())
+            wyzwania.addAll(wyzwania_lista)
+        }
+
+        if(intent.getBooleanExtra("KATEGORIA2CHECK", false) == true){
+            val pytania_sport: Array<String> = resources.getStringArray(R.array.pytania_sport)
+            val pytania_lista = pytania_sport.toCollection(ArrayList())
+            pytania.addAll(pytania_lista)
+
+            val wyzwania_sport: Array<String> = resources.getStringArray(R.array.wyzwania_sport)
+            val wyzwania_lista = wyzwania_sport.toCollection(ArrayList())
+            wyzwania.addAll(wyzwania_lista)
+        }
+
+        if(intent.getBooleanExtra("KATEGORIA3CHECK", false) == true){
+            val pytania_plus18: Array<String> = resources.getStringArray(R.array.pytania_plus18)
+            val pytania_lista = pytania_plus18.toCollection(ArrayList())
+            pytania.addAll(pytania_lista)
+
+            val wyzwania_plus18: Array<String> = resources.getStringArray(R.array.wyzwania_plus18)
+            val wyzwania_lista = wyzwania_plus18.toCollection(ArrayList())
+            wyzwania.addAll(wyzwania_lista)
+        }
+
+        if(intent.getBooleanExtra("KATEGORIA4CHECK", false) == true){
+            val pytania_rodzinne: Array<String> = resources.getStringArray(R.array.pytania_rodzinne)
+            val pytania_lista = pytania_rodzinne.toCollection(ArrayList())
+            pytania.addAll(pytania_lista)
+
+            val wyzwania_rodzinne: Array<String> = resources.getStringArray(R.array.wyzwania_rodzinne)
+            val wyzwania_lista = wyzwania_rodzinne.toCollection(ArrayList())
+            wyzwania.addAll(wyzwania_lista)
+        }
+
+        var losowe = Random.nextInt(0, pytania.size-1)
         var losowe1 = 1
         var check = 0
 
         val animation1 = AnimationUtils.loadAnimation(this, R.anim.zwieksz)
         val animation2 = AnimationUtils.loadAnimation(this, R.anim.zmniejsz)
 
-        binding.karta.text = pytania_lista[losowe]
+        binding.karta.text = pytania[losowe]
         binding.nieWykonane.setOnClickListener(){
-            if(pytania_lista.isNotEmpty()){
-                pytania_lista.removeAt(losowe)
-                if(pytania_lista.size == 1){
-                    binding.karta.text = pytania_lista[0]
-                    pytania_lista.clear()
+            if(pytania.isNotEmpty()){
+                pytania.removeAt(losowe)
+                if(pytania.size == 1){
+                    binding.karta.text = pytania[0]
+                    pytania.clear()
 
                 }
                 else{
-                    losowe = Random.nextInt(0, pytania_lista.size-1)
-                    binding.karta.text = pytania_lista[losowe]
+                    losowe = Random.nextInt(0, pytania.size-1)
+                    binding.karta.text = pytania[losowe]
                 }
             }else{binding.karta.text = "Koniec Gry!"}
         }
 
         binding.karta.setOnClickListener {
-            if (intent.getBooleanExtra("REVERSE",false == true)){
-                if (wyzwania_lista.isNotEmpty()) {
-                    if (wyzwania_lista[losowe1] != binding.karta.text) {
+            if (intent.getBooleanExtra("REVERSE",false) == true){
+                if (wyzwania.isNotEmpty()) {
+                    if (wyzwania[losowe1] != binding.karta.text) {
                         binding.karta.startAnimation(animation1)
                         animation1.setAnimationListener(object : Animation.AnimationListener {
                             override fun onAnimationStart(animation: Animation?) {}
 
                             override fun onAnimationEnd(animation: Animation?) {
                                 if (check == 1) {
-                                    wyzwania_lista.removeAt(losowe1)
+                                    wyzwania.removeAt(losowe1)
                                 }
 
-                                if (wyzwania_lista.size == 1) {
+                                if (wyzwania.size == 1) {
                                     binding.karta.startAnimation(animation2)
-                                    binding.karta.text = wyzwania_lista[0]
-                                    wyzwania_lista.clear()
+                                    binding.karta.text = wyzwania[0]
+                                    wyzwania.clear()
 
                                 } else {
-                                    losowe1 = Random.nextInt(0, wyzwania_lista.size - 1)
+                                    losowe1 = Random.nextInt(0, wyzwania.size - 1)
                                     binding.karta.startAnimation(animation2)
-                                    binding.karta.text = wyzwania_lista[losowe1]
+                                    binding.karta.text = wyzwania[losowe1]
                                     check = 1
                                 }
                             }
--- app/src/main/java/pl/jdteam/kartymelanzowe/KategorieAc.kt
+++ app/src/main/java/pl/jdteam/kartymelanzowe/KategorieAc.kt
@@ -12,15 +12,64 @@ class KategorieAc : ComponentActivity() {
         binding = ActivityKategorieBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.kat1.setOnClickListener(){
+            var kategoria1check = binding.kat1.isChecked
+            var kategoria2check = binding.kat2.isChecked
+            var kategoria3check = binding.kat3.isChecked
+            var kategoria4check = binding.kat4.isChecked
+
+            val start_gra = Intent(applicationContext, GameplayAc::class.java)
+            start_gra.putExtra("KATEGORIA1CHECK", kategoria1check)
+            start_gra.putExtra("KATEGORIA2CHECK", kategoria2check)
+            start_gra.putExtra("KATEGORIA3CHECK", kategoria3check)
+            start_gra.putExtra("KATEGORIA4CHECK", kategoria4check)
+
             val przelacz_gameplay = Intent(applicationContext, GraczeAc::class.java)
             startActivity(przelacz_gameplay)
         }
         binding.kat2.setOnClickListener(){
+            var kategoria1check = binding.kat1.isChecked
+            var kategoria2check = binding.kat2.isChecked
+            var kategoria3check = binding.kat3.isChecked
+            var kategoria4check = binding.kat4.isChecked
+
+            val start_gra = Intent(applicationContext, GameplayAc::class.java)
+            start_gra.putExtra("KATEGORIA1CHECK", kategoria1check)
+            start_gra.putExtra("KATEGORIA2CHECK", kategoria2check)
+            start_gra.putExtra("KATEGORIA3CHECK", kategoria3check)
+            start_gra.putExtra("KATEGORIA4CHECK", kategoria4check)
+
             val przelacz_gameplay = Intent(applicationContext, GraczeAc::class.java)
             startActivity(przelacz_gameplay)
         }
         binding.kat3.setOnClickListener(){
+            var kategoria1check = binding.kat1.isChecked
+            var kategoria2check = binding.kat2.isChecked
+            var kategoria3check = binding.kat3.isChecked
+            var kategoria4check = binding.kat4.isChecked
 
+            val start_gra = Intent(applicationContext, GameplayAc::class.java)
+            start_gra.putExtra("KATEGORIA1CHECK", kategoria1check)
+            start_gra.putExtra("KATEGORIA2CHECK", kategoria2check)
+            start_gra.putExtra("KATEGORIA3CHECK", kategoria3check)
+            start_gra.putExtra("KATEGORIA4CHECK", kategoria4check)
+
+            val przelacz_gameplay = Intent(applicationContext, GraczeAc::class.java)
+            startActivity(przelacz_gameplay)
+        }
+        binding.kat4.setOnClickListener(){
+            var kategoria1check = binding.kat1.isChecked
+            var kategoria2check = binding.kat2.isChecked
+            var kategoria3check = binding.kat3.isChecked
+            var kategoria4check = binding.kat4.isChecked
+
+            val start_gra = Intent(applicationContext, GameplayAc::class.java)
+            start_gra.putExtra("KATEGORIA1CHECK", kategoria1check)
+            start_gra.putExtra("KATEGORIA2CHECK", kategoria2check)
+            start_gra.putExtra("KATEGORIA3CHECK", kategoria3check)
+            start_gra.putExtra("KATEGORIA4CHECK", kategoria4check)
+
+            val przelacz_gameplay = Intent(applicationContext, GraczeAc::class.java)
+            startActivity(przelacz_gameplay)
         }
         binding.ustawienia.setOnClickListener(){
             val przelacz_ustawienia = Intent(applicationContext, UstawieniaAc::class.java)
